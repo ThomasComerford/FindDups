7c70bf625813b0fb21c39482bf6afad96ee9cb86
Thomas Comerford <Thomas@Comerford.com>
2014-01-24 22:22:22 +0000

    Added initial code

--- src/FileInfo.java
+++ src/FileInfo.java
@@ -1,4 +1,22 @@
 
 public class FileInfo {
+	
+	private long modifiedTime = 0;
+	private long fileSize = 0;
+	private String path = "";
+	private String md5Hash = "";
+	
+	public FileInfo() {
+		
+	}
+
+	public boolean equals(FileInfo fi) {
+		
+		if(this.fileSize != fi.fileSize)
+			return false;
+		
+		return false;
+
+	}
 
 }

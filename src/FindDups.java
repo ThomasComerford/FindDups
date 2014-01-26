6cbbea793db78ec47aae8d51c61665a0ebf56ea8
Thomas Comerford <Thomas@Comerford.com>
2014-01-26 21:05:36 +0000

    Add main class

---
+++ src/FindDups.java
@@ -0,0 +1,20 @@
+
+
+public class FindDups {
+
+	public static void main(String[] args) {
+		
+
+		for(int i = 0;i < args.length;i++) {
+			System.out.println(args[i]);
+		}
+		
+		// for all args
+			// find all files & get info
+		
+		// search through data structure
+		// if filesize is matching, check md5 hash
+	
+	}
+
+}
